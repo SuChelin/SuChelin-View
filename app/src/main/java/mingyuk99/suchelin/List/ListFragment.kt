6cfa48cc4777369c812b21fd61ae1da02cdbb678
mingyuk99 <mingyu5675@gmail.com>
2022-05-16 18:17:44 +0900

    PlanB selected. FAB, ListToMapActivity deleted. dataSet added for data sharing between list with map.

--- app/src/main/java/mingyuk99/suchelin/List/ListFragment.kt
+++ app/src/main/java/mingyuk99/suchelin/List/ListFragment.kt
@@ -1,6 +1,5 @@
 package mingyuk99.suchelin.List
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import mingyuk99.suchelin.ListToMapActivity
 import mingyuk99.suchelin.R
 import mingyuk99.suchelin.dataSet
 
@@ -30,20 +26,16 @@ class ListFragment : Fragment() {
 
         val rv = view.findViewById<RecyclerView>(R.id.rv)
         val items = mutableListOf<dataSet>()
-        val btnGoToMap = view.findViewById<FloatingActionButton>(R.id.btnGoToMap)
 
-        val tempDBtest = dataSet("https://search.pstatic.net/common/?autoRotate=true&quality=95&type=f180_180&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210302_125%2F161464487124061agC_JPEG%2FK15utTFWXeuNEny1JMXiV57W.jpg","할리스 수원대학교점","main")
+        val dummyDB = mutableListOf<dataSet>()
+        dummyDB.add(dataSet("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAGJ28R7vVAaVouy37LhbNlptqTJQwl208Vg&usqp=CAU","던킨도너츠 수원대점","바바리안 도넛",37.214523,126.978058))
+        dummyDB.add(dataSet("https://search.pstatic.net/common/?autoRotate=true&quality=95&type=f180_180&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210302_125%2F161464487124061agC_JPEG%2FK15utTFWXeuNEny1JMXiV57W.jpg","할리스 수원대학교점","토피넛 라떼",37.214367, 126.978968))
+        dummyDB.add(dataSet("https://mblogthumb-phinf.pstatic.net/MjAyMDAzMTdfOTAg/MDAxNTg0NDI4ODk4MTE3.Sbya0oZBVcS1uXbtrwTNdetrqjvx0Y3FHpBkdjCEELkg.olDkDm0beJhdPP3hDzesjGT4HX20mN4ILAISylHrccUg.JPEG.hyesun0305/1584428897220.jpg?type=w800","와우당","main"))
 
-        btnGoToMap.setOnClickListener {
-            val intent = Intent(context, ListToMapActivity::class.java)
-            startActivity(intent)
-            //공통의 로컬db를 사용해서 처리
-            //위치,가게이름,대표메뉴가 사용될예정
-        }
-
-        items.add(dataSet("https://search.pstatic.net/common/?autoRotate=true&quality=95&type=f180_180&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210302_125%2F161464487124061agC_JPEG%2FK15utTFWXeuNEny1JMXiV57W.jpg","할리스 수원대학교점","main", "토피넛 라떼"))
-        items.add(dataSet("https://mblogthumb-phinf.pstatic.net/MjAyMDAzMTdfOTAg/MDAxNTg0NDI4ODk4MTE3.Sbya0oZBVcS1uXbtrwTNdetrqjvx0Y3FHpBkdjCEELkg.olDkDm0beJhdPP3hDzesjGT4HX20mN4ILAISylHrccUg.JPEG.hyesun0305/1584428897220.jpg?type=w800","와우당","main"))
-        items.add(tempDBtest)
+        items.add(dummyDB[0])
+        items.add(dummyDB[1])
+        items.add(dummyDB[2])
+        items.add(dataSet("imageUrl","dummy","main"))
         items.add(dataSet("imageUrl","dummy","main"))
         items.add(dataSet("imageUrl","dummy","main"))
         items.add(dataSet("imageUrl","dummy","main"))
--- app/src/main/java/mingyuk99/suchelin/dataSet.kt
+++ app/src/main/java/mingyuk99/suchelin/dataSet.kt
@@ -3,8 +3,9 @@ package mingyuk99.suchelin
 data class dataSet(
     val imageUrl: String = "",
     val name: String = "",
-    val location: String = "",
-    val detail: String = ""
+    val detail: String = "",
+    val latitude : Double = 0.0,
+    val longitude : Double = 0.0
 //detail
 //distance
 )

387f4ee3aff39b75d6e90c0429f2991845f460f9
qscwdve <zlzldbsgml@naver.com>
2022-06-06 18:53:47 +0900

    vote dialog

--- app/src/main/java/Guide/suchelin/Vote/VoteDialog.kt
+++ app/src/main/java/Guide/suchelin/Vote/VoteDialog.kt
@@ -1,15 +1,16 @@
 package Guide.suchelin.Vote
 
 import Guide.suchelin.databinding.DialogVoteBinding
+import android.app.Activity
 import android.graphics.Color
+import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.Window
+import android.view.*
 import androidx.fragment.app.DialogFragment
+
 
 class VoteDialog(val storeTitle: String, val voteControl: VoteControl) : DialogFragment() {
     private lateinit var binding : DialogVoteBinding
@@ -23,17 +24,21 @@ class VoteDialog(val storeTitle: String, val voteControl: VoteControl) : DialogF
         // 모서리 둥글게
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         isCancelable = false
-
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // 가게 이름 설정
-        val storeName = "\u003C ${storeTitle} \u003E"
-        binding.voteDialogStoreTitleTextView.text = storeName
+        binding.voteDialogStoreTitleTextView.text = storeTitle
 
         // 취소하기
         binding.voteDialogCancelImageView.setOnClickListener {
